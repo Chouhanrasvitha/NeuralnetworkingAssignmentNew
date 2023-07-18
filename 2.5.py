b57f5ca3d80f94b187010bdb6493608679b9ac72
YOUR_USERNAME <chouhanrasvitha@gmail.com>
2023-07-18 09:25:49 -0500

    NeuralNetworking

---
+++ 2.5.py
@@ -0,0 +1,93 @@
+from keras import Sequential
+import numpy as np
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.datasets import mnist
+
+from keras.utils import to_categorical
+import matplotlib.pyplot as plt
+
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+print(train_images.shape[1:])
+# process the data
+# 1. convert each image of shape 28*28 to 784 dimensional which will be fed to the network as a single feature
+dimData = np.prod(train_images.shape[1:])
+print(dimData)
+train_data = train_images.reshape(train_images.shape[0], dimData)
+test_data = test_images.reshape(test_images.shape[0], dimData)
+
+# convert data to float and scale values between 0 and 1
+train_data = train_data.astype('float')
+test_data = test_data.astype('float')
+# scale data
+train_data /= 255.0
+test_data /= 255.0
+# change the labels frominteger to one-hot encoding. to_categorical is doing the same thing as LabelEncoder()
+train_labels_one_hot = to_categorical(train_labels)
+test_labels_one_hot = to_categorical(test_labels)
+
+# creating network
+model = Sequential()
+model.add(Dense(512, activation='relu', input_shape=(dimData,)))
+model.add(Dense(512, activation='relu'))
+model.add(Dense(10, activation='softmax'))
+# Replace the undefined variables with the correct ones
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+history = model.fit(train_data, train_labels_one_hot, batch_size=256, epochs=10, verbose=1,
+                    validation_data=(test_data, test_labels_one_hot))
+
+# create a list of models to train
+models = []
+num_classes = 10
+
+# model with 1 hidden layer and tanh activation
+model = Sequential()
+model.add(Dense(512, activation='tanh', input_shape=(784,)))
+model.add(Dropout(0.2))
+model.add(Dense(num_classes, activation='softmax'))
+models.append(('1 hidden layer with tanh', model))
+
+# model with 1 hidden layer and sigmoid activation
+model = Sequential()
+model.add(Dense(512, activation='sigmoid', input_shape=(784,)))
+model.add(Dropout(0.2))
+model.add(Dense(num_classes, activation='softmax'))
+models.append(('1 hidden layer with sigmoid', model))
+
+# model with 2 hidden layers and tanh activation
+model = Sequential()
+model.add(Dense(512, activation='tanh', input_shape=(784,)))
+model.add(Dropout(0.2))
+model.add(Dense(512, activation='tanh'))
+model.add(Dropout(0.2))
+model.add(Dense(num_classes, activation='softmax'))
+models.append(('2 hidden layers with tanh', model))
+
+# model with 2 hidden layers and sigmoid activation
+model = Sequential()
+model.add(Dense(512, activation='sigmoid', input_shape=(784,)))
+model.add(Dropout(0.2))
+model.add(Dense(512, activation='sigmoid'))
+model.add(Dropout(0.2))
+model.add(Dense(num_classes, activation='softmax'))
+models.append(('2 hidden layers with sigmoid', model))
+
+# train each model and plot loss and accuracy curves
+for name, model in models:
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    history = model.fit(train_data, train_labels_one_hot, validation_data=(test_data, test_labels_one_hot),
+                        epochs=20, batch_size=128, verbose=0)
+    # plot loss and accuracy curves
+    plt.plot(history.history['loss'], label='train_loss')
+    plt.plot(history.history['val_loss'], label='val_loss')
+    plt.plot(history.history['accuracy'], label='train_accuracy')
+    plt.plot(history.history['val_accuracy'], label='val_accuracy')
+    plt.title(name)
+    plt.xlabel('Epoch')
+    plt.legend()
+    plt.show()
+
+    # evaluate the model on test data
+    loss, accuracy = model.evaluate(test_data, test_labels_one_hot, verbose=0)
+    print('{} - Test loss: {:.4f}, Test accuracy: {:.4f}'.format(name, loss, accuracy))
